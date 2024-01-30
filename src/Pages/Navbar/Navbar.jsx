d43703239208928998b1604d0f77d409fa3e148e
Ariful Islam <hello.mrariful@gmail.com>
2024-01-30 14:50:23 +0600

    add react scroll on navbar

--- src/Pages/Navbar/Navbar.jsx
+++ src/Pages/Navbar/Navbar.jsx
@@ -1,4 +1,9 @@
-import { useState } from 'react';
+import { useState } from "react";
+import "./Navbar.css";
+import {
+  Link,
+} from "react-scroll";
+
 const Navbar = () => {
   const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
   const toggleMobileMenu = () => {
@@ -10,11 +15,19 @@ const Navbar = () => {
       <nav className="bg-white border-gray-200 dark:bg-gray-900">
         <div className="max-w-screen-xl flex flex-wrap items-center justify-between mx-auto p-4">
           <a className="flex items-center space-x-3 rtl:space-x-reverse">
-            <span className="self-center font-semibold whitespace-nowrap dark:text-white md:text-4xl text-2xl">Ariful <span className=' text-[#f8024d]'>Islam</span></span>
+            <span className="self-center font-semibold whitespace-nowrap dark:text-white md:text-4xl text-2xl">
+              Ariful <span className=" text-[#f8024d]">Islam.</span>
+            </span>
           </a>
           <div className="flex md:order-2 space-x-3 md:space-x-0 rtl:space-x-reverse">
-            <a className='bg-[#f8024d] text-white rounded md:py-3 md:px-4 py-2 px-2' href="https://github.com/hellomrariful"> See Github</a>
-            
+            <a
+              className="bg-[#f8024d] text-white rounded md:py-3 md:px-4 py-2 px-2"
+              href="https://github.com/hellomrariful"
+            >
+              {" "}
+              See Github
+            </a>
+
             <button
               type="button"
               onClick={toggleMobileMenu}
@@ -30,57 +43,82 @@ const Navbar = () => {
                 fill="none"
                 viewBox="0 0 17 14"
               >
-                <path stroke="currentColor" d="M1 1h15M1 7h15M1 13h15"/>
+                <path stroke="currentColor" d="M1 1h15M1 7h15M1 13h15" />
               </svg>
             </button>
           </div>
           <div
             className={`items-center justify-between w-full md:flex md:w-auto md:order-1 ${
-              isMobileMenuOpen ? '' : 'hidden'
+              isMobileMenuOpen ? "" : "hidden"
             }`}
             id="navbar-cta"
           >
             <ul className="flex flex-col font-medium p-4 md:p-0 mt-4 border border-gray-100 rounded-lg bg-gray-50 md:space-x-8 rtl:space-x-reverse md:flex-row md:mt-0 md:border-0 md:bg-white dark:bg-gray-800 md:dark:bg-gray-900 dark:border-gray-700">
               <li>
-                <a
-                  href="#"
-                  className="block py-2 px-3 md:p-0 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:dark:text-blue-500"
-                  aria-current="page"
+              <Link
+                  className="hover:text-white hover:bg-[#f8024d] py-3 px-3 rounded"
+                  href="#home"
+                  to="home"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={1500}
                 >
                   Home
-                </a>
+                </Link>
               </li>
               <li>
-                <a
+              <Link
+                  className="hover:text-white hover:bg-[#f8024d] py-3 px-3 rounded"
                   href="#about"
-                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  to="about"
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={1500}
                 >
                   About
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#projects"
-                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 d:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              <Link
+                  className="hover:text-white hover:bg-[#f8024d] py-3 px-3 rounded"
+                  href="#Projects"
+                  to="projects"
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={1500}
                 >
                   Projects
-                </a>
+                </Link>
               </li>
               <li>
-                <a
-                  href="#skill"
-                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+              <Link
+                  className="hover:text-white hover:bg-[#f8024d] py-3 px-3 rounded"
+                  href="#skills"
+                  to="skill"
+                  spy={true}
+                  smooth={true}
+                  offset={-50}
+                  duration={1500}
                 >
                   Skills
-                </a>
+                </Link>
+               
               </li>
               <li>
-                <a
+                <Link
+                  className="hover:text-white hover:bg-[#f8024d] py-3 px-3 rounded"
                   href="#contact"
-                  className="block py-2 px-3 md:p-0 text-gray-900 rounded hover:bg-gray-100 md:hover:bg-transparent md:hover:text-blue-700 md:dark:hover:text-blue-500 dark:text-white dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700"
+                  to="contact"
+                  spy={true}
+                  smooth={true}
+                  offset={50}
+                  duration={1500}
                 >
                   Contact
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
@@ -91,4 +129,3 @@ const Navbar = () => {
 };
 
 export default Navbar;
-
